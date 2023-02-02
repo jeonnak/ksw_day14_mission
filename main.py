64f3a221c6c3a1f2fa08dd674328f62ae586c9e1
JNK <deal6789@naver.com>
2023-02-02 18:13:41 +0900

    mission 10.8

--- main.py
+++ main.py
@@ -1,40 +1,46 @@
 """
-10.7
-print(hydrogen)을 호출한다. Element 클래스의 정의에서 dump 메서드를
- __str__() 메서드로 바꿔서 새로운 hydrogen 객체를 생성하고,
- 그리고 print(hydrogen)을 다시 호출한다.
+10.8 Element 클래스를 수정해서 name, symbol, number의 속성을
+ private로 만든다. 각 속성값을 반환하기 위해 getter 프로퍼티를 정의한다.
 """
 
-# name, symbol, number 인스턴스 속성 가진 Element 클래스 생성
+# name, symbol, number 인스턴스 private 속성 가지도록 Element 클래스 수정
 class Element():
     def __init__(self, name, symbol, number):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
+        self.hidden__name = name
+        self.hidden__symbol = symbol
+        self.hidden__number = number
 
- # name, symbol, number 속성의 값을 출력하는 dump() 메서드
+    @property
+    def name(self):
+        print('getter')
+        return self.hidden__name
 
-   # def dump(self):
-   #     print(self.name, self.symbol, self.number)
+    @name.setter
+    def name(self, name):
+        print('setter')
+        self.hidden__name = name
 
-# __str__() 메서드로 바꾸기
-    def __str__(self):
-         return f'{self.name} {self.symbol} {self.number}'
+    @property
+    def symbol(self):
+        print('getter')
+        return self.hidden__symbol
 
-# 'name' : 'Hydrogen', 'symbol' : 'H', 'number' : 1과 같이
-#  키와 값으로 이루어진 el_dict 딕셔너리 생성
+    @symbol.setter
+    def symbol(self, symbol):
+        print('setter')
+        self.hidden__symbol = symbol
 
-el_dict = {'name' : 'Hydrogen', 'symbol' : 'H', 'number' : 1}
+    @property
+    def number(self):
+        print('getter')
+        return self.hidden__number
 
-# el_dict 딕셔너리로부터 Element 클래스의 hydrogen 객체를 생성
+    @number.setter
+    def number(self, number):
+        print('setter')
+        self.hidden__number = number
 
-hydrogen = Element(el_dict['name'], el_dict['symbol'], el_dict['number'])
-print(hydrogen.name, hydrogen.symbol, hydrogen.number)
-
-# hydrogen.dump()
-
-hydrogen.__str__()
-
-# dump() 메서드 일 때 print(hydrogen) 호출 결과 : <__main__.Element object at 0x000001BF8488CB90>
-# __str__() 메서드 일 때 print(hydrogen) 호출 결과 : Hydrogen H 1
-print(hydrogen)
+produce = Element('Hydrogen', 'H', 1)
+produce.name
+produce.symbol
+produce.number
